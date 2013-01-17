7e53a9519c4d6d01f0451785a610a7bd1c95628b
Anna S Karpoja <annakarp@dhp-50.cs.helsinki.fi>
2013-01-17 14:11:24 +0200

    ensimmäiset luokat

---
+++ OhHa/src/ohha/Tietovisa.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohha;
+
+/**
+ *
+ * @author annakarp
+ */
+public class Tietovisa {
+    private String nimimerkki;
+    
+    public Tietovisa(String nimimerkki){
+        this.nimimerkki = nimimerkki;
+    
+}
+}
